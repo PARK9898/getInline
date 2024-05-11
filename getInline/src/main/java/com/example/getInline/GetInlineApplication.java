01689304c9d5a4df8998e2c71da70e3133035b5a
PARK9898 <wjddjs21@naver.com>
2024-05-11 16:04:11 +0900

    project setup

---
+++ getInline/src/main/java/com/example/getInline/GetInlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.getInline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetInlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetInlineApplication.class, args);
+	}
+
+}
---
+++ getInline/src/test/java/com/example/getInline/GetInlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getInline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
