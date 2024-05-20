49fc4c3f18e75ed2a8c12c6f3e712a968fbbc7cf
PARK9898 <wjddjs21@naver.com>
2024-05-20 16:30:44 +0900

    Add #2 - PlaceDto 설계

---
+++ getInline/src/main/java/com/example/getInline/dto/PlaceResponse.java
@@ -0,0 +1,20 @@
+package com.example.getInline.dto;
+
+import com.example.getInline.constant.PlaceType;
+
+public record PlaceResponse(PlaceType placeType,
+                            String placeName,
+                            String address,
+                            String phoneNumber,
+                            Integer capacity,
+                            String memo) {
+    public static PlaceResponse of(
+            PlaceType placeType,
+            String placeName,
+            String address,
+            String phoneNumber,
+            Integer capacity,
+            String memo) {
+        return new PlaceResponse(placeType, placeName, address, phoneNumber, capacity, memo);
+    }
+}
